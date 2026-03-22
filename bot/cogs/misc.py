2cbec09498f113332a7957e91499b2dbc49e0b57
Samuel Verrier <samuelverrier@gmail.com>
2026-03-22 19:32:33 -0300

    testing registration process

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -125,14 +125,19 @@ class Misc(commands.Cog):
                     )
                 )
                 return
+            else:
 
-            # Register + link
-            q_insert = """
-                INSERT INTO registrations (user_id, discordname, discordid, registrationdate)
-                VALUES ($1, $2, $3, NOW())
-                ON CONFLICT (user_id) DO NOTHING
-            """
-            await self.bot.db.executeParametrized(q_insert, user_id, discord_name, discord_id)
+                # Register + link
+                q_insert = """
+                    INSERT INTO registrations (user_id, discordname, discordid, registrationdate)
+                    VALUES ($1, $2, $3, NOW())
+                    ON CONFLICT (user_id) DO NOTHING
+                """
+                await self.bot.db.executeParametrized(q_insert, user_id, discord_name, discord_id)
+
+            q_register = f"CALL public.register_user($1)"
+
+            await self.bot.db.executeParametrized(q_insert, user_id)
 
             await status_msg.edit(content=f"✅ Registered user ID `{user_id}` and linked your Discord account.")
             return
